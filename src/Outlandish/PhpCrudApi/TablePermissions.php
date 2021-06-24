7378ab98cc857d0a2b54c867e8efefdc52733203
Harry <harry@outlandish.com>
2021-06-24 18:13:10 +0100

    making classes static

--- src/Outlandish/PhpCrudApi/TablePermissions.php
+++ src/Outlandish/PhpCrudApi/TablePermissions.php
@@ -4,40 +4,130 @@ namespace Outlandish\PhpCrudApi;
 
 class TablePermissions
 {
-    const READ_COLUMNS = [];
-    const WRITE_COLUMNS = [];
-    const LIST_COLUMNS = [];
-    const UPDATE_COLUMNS = [];
-    const DELETE_COLUMNS = [];
-    const INCREMENT_COLUMNS = [];
+    const TABLE_NAME = "";
+    
+    private const read_columns = [];
+    private const list_columns = [];
+    private const update_columns = [];
+    private const delete_columns = [];
+    private const increment_columns = [];
+
+    /**
+     * @return array
+     */
+    public static function getReadColumns(): array
+    {
+        return self::read_columns;
+    }
+
+
+    /**
+     * @return array
+     */
+    public static function getListColumns(): array
+    {
+        return self::list_columns;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getUpdateColumns(): array
+    {
+        return self::update_columns;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getDeleteColumns(): array
+    {
+        return self::delete_columns;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getIncrementColumns(): array
+    {
+        return self::increment_columns;
+    }
 
 
 }
 
 /* and then in your app you'd have */
 
+
 class PetsPermissions extends TablePermissions
 {
-    public const READ_COLUMNS = [
+    private const read_columns = [
+        'id',
+        'owner',
+        'favourite_food',
+        'species',
+    ];
+    private const write_columns = [
         'id',
         'owner',
         'favourite_food',
         'species',
     ];
 
-    public const WRITE_COLUMNS = [
-        'id',
-        'owner',
-        'favourite_food',
-        'species',
-    ];
+    public static function getReadColumns(): array
+    {
+        return self::read_columns;
+    }
+
+
+    /**
+     * @return array
+     */
+    public static function getListColumns(): array
+    {
+        return self::read_columns;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getUpdateColumns(): array
+    {
+        return self::update_columns;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getDeleteColumns(): array
+    {
+        return self::write_columns;
+    }
+
+    /**
+     * @return array
+     */
+    public static function getIncrementColumns(): array
+    {
+        $increment = self::read_columns;
+        $increment[] = 'favourites';
+        return $increment;
+    }
+    
 }
+
 
 class UserPermissions extends TablePermissions
 {
-    public const READ_COLUMNS = [
+    private const read_columns = [
         'id',
-        'display_name',
+        'owner',
+        'favourite_food',
+        'species',
     ];
+    public static function getListColumns(): array
+    {
+        return self::read_columns;
+    }
 
 }
