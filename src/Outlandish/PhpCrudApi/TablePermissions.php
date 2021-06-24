551e8c5b6f163dac46763d5063319b00ca9d9968
Harry <harry@outlandish.com>
2021-06-24 17:56:13 +0100

    WIP - discussion/POC for how table permissions could be managed with classes rather than arrays of arrays of arrays

---
+++ src/Outlandish/PhpCrudApi/TablePermissions.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace Outlandish\PhpCrudApi;
+
+class TablePermissions
+{
+    const READ_COLUMNS = [];
+    const WRITE_COLUMNS = [];
+    const LIST_COLUMNS = [];
+    const UPDATE_COLUMNS = [];
+    const DELETE_COLUMNS = [];
+    const INCREMENT_COLUMNS = [];
+
+
+}
+
+class PetsPermissions extends TablePermissions
+{
+    public const READ_COLUMNS = [
+        'id',
+        'owner',
+        'favourite_food',
+        'species',
+    ];
+
+    public const WRITE_COLUMNS = [
+        'id',
+        'owner',
+        'favourite_food',
+        'species',
+    ];
+}
+class UserPermissions extends TablePermissions
+{
+    public const READ_COLUMNS = [
+        'id',
+        'display_name',
+    ];
+
+}
